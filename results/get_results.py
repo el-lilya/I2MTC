91a63c65624a606ffef27e2bd8ff31d1faaead1b
el-lilya <l.lemikhova@gmail.com>
2021-11-12 20:00:13 +0300

    changed tensorboard: accuracy plot, changed directories' names

--- results/get_results.py
+++ results/get_results.py
@@ -2,12 +2,19 @@ from process_results_utils import get_mean_std_acc, plot_train_images, plot_fals
 
 
 def main():
-    # get_results('hparams_table_max_k=5.csv', 'results_max_k=5.csv')
-    k = 5
-    exp = 3
-    labels = [8, 1, 13, 6]
-    plot_false_predictions('false_predictions.csv', k=k, exp=exp, labels=labels)
-    plot_train_images(k=k, exp=exp, labels=labels)
+    log_dir = 'runs_gpu'
+    # get mean and std accuracy
+    max_k = 7
+    get_mean_std_acc(f'hparams_table_max_k={max_k}.csv', f'results_max_k={max_k}.csv', log_dir=log_dir)
+
+    # plot false predictions
+    k = 7
+    exp = 0
+    labels = [1, 19]
+    plot_false_predictions('false_predictions.csv', k=k, exp=exp, labels=labels, log_dir=log_dir)
+
+    # plot train images
+    plot_train_images(k=k, exp=exp, labels=labels, log_dir=log_dir)
 
 
 if __name__ == "__main__":
--- results/process_results_utils.py
+++ results/process_results_utils.py
@@ -7,16 +7,16 @@ import datetime
 import math
 
 
-def plot_false_predictions(predictions: str, k: int, exp: int, labels, log_dir='../runs/predictions',
+def plot_false_predictions(predictions: str, k: int, exp: int, labels, log_dir='../runs',
                            root='..', data_dir='data/classification_20_clean'):
     name_time = datetime.datetime.now().strftime('%d%h_%I_%M')
-    tracker = TensorboardExperiment(log_path=log_dir + f'/k={k}_exp={exp}_labels={labels}/{name_time}')
+    tracker = TensorboardExperiment(log_path=log_dir + f'/false_preds/k={k}_exp#{exp}_labels={labels}/{name_time}')
     df = pd.read_csv(predictions)
     df = df[(df['k'] == k) & (df['experiment'] == exp) & (df['label'].isin(labels))]
     df.reset_index(inplace=True)
     imgs = [Image.open(os.path.join(root, data_dir, img_path)) for img_path in df['path']]
     ncols = math.ceil(math.sqrt(len(imgs)))
-    fig = plt.figure(figsize=(10, 10))
+    fig = plt.figure(figsize=(8, 6))
     st = fig.suptitle(f"True label : False predictions", fontsize="x-large")
     for i, img in enumerate(imgs):
         ax = plt.subplot(len(imgs) // ncols + 1, ncols, i + 1)
@@ -31,18 +31,17 @@ def plot_false_predictions(predictions: str, k: int, exp: int, labels, log_dir='
     tracker.flush()
 
 
-def plot_train_images(k: int, exp: int, labels, log_dir='../runs/train_images',
+def plot_train_images(k: int, exp: int, labels, log_dir='../runs',
                       root='..', split_dir='splits', data_dir='data/classification_20_clean'):
     name_time = datetime.datetime.now().strftime('%d%h_%I_%M')
-    name_labels = '_'.join(labels)
-    tracker = TensorboardExperiment(log_path=log_dir + f'/k={k}_exp={exp}_label={name_labels}/{name_time}')
+    tracker = TensorboardExperiment(log_path=log_dir + f'/train_images/k={k}_exp#{exp}_labels={labels}/{name_time}')
     df = pd.read_csv(os.path.join(root, split_dir, f'train_k{k}_#{exp}'))
     # df = df.sort_values('label')
     df = df[df['label'].isin(labels)]
     df.reset_index(inplace=True)
     imgs = [Image.open(os.path.join(root, data_dir, img_path)) for img_path in df['img_path']]
     ncols = 5
-    fig = plt.figure(figsize=(10,10))
+    fig = plt.figure(figsize=(8, 6))
     st = fig.suptitle(f"Images in train dataset for k={k}, exp={exp}, labels={labels}", fontsize="x-large")
     for i, img in enumerate(imgs):
         ax = plt.subplot(len(imgs) // ncols + 1, ncols, i + 1)
@@ -53,20 +52,32 @@ def plot_train_images(k: int, exp: int, labels, log_dir='../runs/train_images',
     fig.tight_layout()
     st.set_y(0.95)
     fig.subplots_adjust(top=0.85)
-    plt.show()
+    # plt.show()
     tracker.add_figure(f'TRAIN IMAGES', fig)
     tracker.flush()
 
 
-def get_mean_std_acc(name: str, name_csv: str):
+def get_mean_std_acc(name: str, name_csv: str, log_dir='../runs'):
     results_full = pd.read_csv(name)
     results = pd.DataFrame({'k': results_full['k'].unique()})
     dict_mean = results_full.groupby('k')['test_accuracy'].mean().to_dict()
     dict_std = results_full.groupby('k')['test_accuracy'].std().to_dict()
     results['mean'] = results['k'].map(dict_mean)
     results['std'] = results['k'].map(dict_std)
-    print(results)
+    # print(results)
     results.to_csv(name_csv)
+
+    max_k = results['k'].max()
+    name_time = datetime.datetime.now().strftime('%d%h_%I_%M')
+    tracker = TensorboardExperiment(log_path=log_dir + f'/accuracy/max_k={max_k}/{name_time}')
+    fig = plt.figure(figsize=(8, 6))
+    plt.plot(results['k'], results['mean'])
+    plt.title('Mean accuracy')
+    plt.xlabel('k')
+    plt.ylabel('Mean accuracy')
+    plt.grid()
+    tracker.add_figure(f'ACCURACY', fig)
+    tracker.flush()
 
 
 def main():
